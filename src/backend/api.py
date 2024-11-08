04957f4aec6f16abace29e58abff81c2a65e79f1
ioSoyPato <patrikwars@gmail.com>
2024-11-07 18:24:16 -0600

    ready

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,64 +1,31 @@
-import pickle
 import mlflow
-from fastapi import FastAPI
-from pydantic import BaseModel
-from mlflow import MlflowClient
+import pandas as pd
 
-dagshub_repo = "https://dagshub.com/ioSoyPato/proyecto-final-ciencia-datos"
+# Configura la URI de rastreo de mlflow (cambia esto si necesitas otra dirección)
 MLFLOW_TRACKING_URI = "https://dagshub.com/ioSoyPato/proyecto-final-ciencia-datos.mlflow"
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-
-mlflow.set_tracking_uri(uri=MLFLOW_TRACKING_URI)
-client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
-
-run_ = mlflow.search_runs(order_by=['metrics.rmse ASC'],
-                          output_format="list",
-                          experiment_names=["temp-prediction-experiment"]
-                          )[0]
-
-
-run_id = run_.info.run_id
-
-run_uri = f"runs:/{run_id}/preprocessor"
-
-
-client.download_artifacts(
-    run_id=run_id,
-    path='preprocessor',
-    dst_path='.'
-)
-
-with open("preprocessor/preprocessor.b", "rb") as f_in:
-    dv = pickle.load(f_in)
-
-
+# Nombre del modelo y alias
 model_name = "MyModel"
 alias = "champion"
 
+# URI del modelo
 model_uri = f"models:/{model_name}@{alias}"
 
-champion_model = mlflow.pyfunc.load_model(
-    model_uri=model_uri
-)
+# Carga el modelo
+champion_model = mlflow.pyfunc.load_model(model_uri=model_uri)
 
-def predict(input_data):
-
-    X_pred = (input_data)
-
-    return champion_model.predict(X_pred)
-
-app = FastAPI()
-
-class InputData(BaseModel):
-    PULocationID: str
-    DOLocationID: str
-    trip_distance: float
+input_data = pd.DataFrame({
+    "ts": [1594512094.9],
+    "co": [0.001171],
+    "humidity": [1.1],
+    "lpg": [0.002693],
+    "smoke": [0.006692]
+})
 
 
-@app.post("/predict")
-def predict_endpoint(input_data: InputData):
-    result = predict(input_data)[0]
+# Realiza la predicción
+prediction = champion_model.predict(input_data)
 
-    return {
-        "prediction": float(result)
-    }
+# Imprime la predicción
+print("Predicción:", prediction)
